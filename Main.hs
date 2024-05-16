74af482135be2624aaf15ddb5941da6ffb145455
V. Jackson <vjackson725@users.noreply.github.com>
2024-05-16 20:07:04 +1000

    check for file on run

--- Main.hs
+++ Main.hs
@@ -1,12 +1,20 @@
 module Main where
 
+import Control.Applicative (empty)
+import Control.Monad (when)
+
 import System.Environment
 import Kuifje.Run
 
-main = do args <- getArgs
-          let file = head args
-              flags = tail args
-           in  runFileDefaultParams file flags
+main =
+  do
+    args <- getArgs
+    if null args
+    then putStrLn "provide a file"
+    else 
+      let file = head args
+          flags = tail args
+       in runFileDefaultParams file flags
 --           in do runHyper file flags
 --          case args of
 --            [file]    -> do runHyper file
